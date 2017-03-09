1e155a9473b4e00406e29b059a9cb88584b75a9b
302-MODULO <302-MODULO>
2017-03-08 21:56:32 -0500

    Sem 01

---
+++ Sem01/libreria.js
@@ -0,0 +1,14 @@
+// Implementacion de la logica
+
+function sumar(n1, n2){
+	return (n1 + n2);
+}
+
+function factorial( n ){
+	var f = 1, i;
+	for(i = 2; i <= n; i++){
+		f *= i;
+	}
+	return f;
+}	
+
